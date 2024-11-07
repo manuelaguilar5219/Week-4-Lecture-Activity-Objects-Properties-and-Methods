7900d5cd67ca8b9c1ef16b355ae712e50d0aaa5b
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-11-06 22:03:06 -0800

    Question 2

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 let myArray = [25, 30, 35, 40, 45]
 console.log(myArray[0]); // Output: 25
 console.log(myArray[4]); // Output: 45
+// 2.
+// Using a for loop to output each of the values in the Array
+for (let i= 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
